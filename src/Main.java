2f28f4a0b721f7f7a544e7e92c9ad2e5264e039d
Artyom Pionov <pionovartem11@gmail.com>
2022-10-09 19:12:10 +0100

    First Commit Challenge Done and Works

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        float pounds = 100f;
+        float rateOfKilogram = 0.45359237f;
+        float result = pounds * rateOfKilogram;
+        System.out.printf("100 pounds in kilos = " + result);
+    }
+}
